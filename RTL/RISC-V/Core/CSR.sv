XuanTongYao/XT_RISC-V_Soc

commit 6e5ab3e0f735a0eae14f9bae3f1d24fd2716fe25

更新ACT4仿真代码，移除了CPP包装器，直接使用--timing模式 优化处理器内核，CSR复位完全遵循标准

RTL/RISC-V/Core/CSR.sv
@@ -17,7 +17,6 @@ module CSR
 
     // 自陷控制接口
     trap_if.csr trap,
-    // input [31:0] new_mtval,
 
     // 中断源
     int_source_if.hart mint,
@@ -50,20 +49,18 @@ module CSR
 
   wire [7:0] short_addr = rwaddr.short_addr;
   wire atomic_rw_en = wen && (stall_n || debug.halted) && rwaddr.privilege_level == MACHINE;
-  // wire atomic_counter_rw_en = csr_wen && stall_n && rw.addr.privilege_level == MACHINE && rw.addr.mode == 2'b10;
 
 
-  //----------机器模式CSR----------//
-  // 信息寄存器
-  // wire [31:0] misa = {CFG.MXL, {CFG.XLEN - 28{1'b0}}, CFG.EXTENSION};  // ISA信息（不实现 只读0）
+  // 信息 寄存器
   // wire [31:0] mvendorid = 32'h31305458;  //  供应商 ID（不实现 只读0）
   // wire [31:0] marchid = 32'h31305458;  // 微架构 ID（不实现 只读0）
   // wire [31:0] mimpid = 32'h31303030;  // 实现版本ID（不实现 只读0）
   wire [31:0] mhartid = 32'h0;  // 硬件线程ID
   // wire [31:0] mconfigptr = 32'h0;  // 配置指针（配置数据不存在 只读0）
 
-  // 自陷寄存器
+  // 自陷设置 寄存器
   mstatus_m_only_t mstatus;  //状态寄存器
+  // wire [31:0] misa = {CFG.MXL, {CFG.XLEN - 28{1'b0}}, CFG.EXTENSION};  // ISA信息（不实现 只读0）
   mtvec_t mtvec;  //自陷处理函数基地址
   mie_m_only_t mie;  //中断启用寄存器
   mip_m_only_t mip;  //挂起(待处理)的中断（只读）
@@ -72,7 +69,7 @@ module CSR
   logic [31:0] mscratch;  // 暂存寄存器
   logic [CFG.PC_LEN-1:0] mepc;  // 异常程序地址
   mcause_t mcause;  // 自陷原因
-  // logic [31:0] mtval;  // 自陷额外信息（只读0实现）
+  // logic [31:0] mtval;  // 自陷额外信息（只读0实现，内核可以读取对应指令获取额外信息）
 
   // 连接自陷控制接口
   assign trap.mstatus = PadMstatus(mstatus);
@@ -100,6 +97,7 @@ module CSR
   end
 
   // 配置（不实现）
+  // 物理内存属性（不实现）
   // 内存保护（不实现）
 
   // 调试触发器模块 Sdtrig扩展（不实现）
@@ -120,7 +118,7 @@ module CSR
             unique case (short_addr)
               8'h14:   rdata = mhartid;
               // 8'h15:   csr_rdata = mconfigptr;
-              default: rdata = 32'b0;
+              default: ;
             endcase
           end
           2'b01:
@@ -150,7 +148,6 @@ module CSR
               8'h40:   rdata = mscratch;
               8'h41:   rdata = CFG.XLEN'(PadPC(mepc, CFG.PC_ZEROS));
               8'h42:   rdata = mcause;
-              // 8'h43:   csr_rdata = mtval;
               8'h44:   rdata = PadMieMip(mip);
               default: ;
             endcase
@@ -163,17 +160,16 @@ module CSR
 
   wire dcsr_t write_dcsr = wdata;
   always_ff @(posedge clk, posedge rst) begin
-    if (rst) begin
-      mstatus <= 0;
-      mie <= 0;
-      mtvec <= 0;
+    if (rst) begin  // 特权级文档 3.4节
+      mstatus <= 0;  // mstatus 字段的 MIE 和 MPRV 被复位到 0
+      mcause <= 0;  // 在不区分复位条件的实现中 mcause 应复位到 0
+      // 其他的状态均为 未定义
       dcsr <= 0;
     end else begin
       if (atomic_rw_en && rwaddr.mode == 2'b00) begin
         unique case (short_addr)
           8'h00:   mstatus <= UnpadMstatus(wdata);
-          8'h04:   mie <= {wdata[11], wdata[7], wdata[3]};
-          8'h05:   mtvec <= wdata;
+          // 8'h42: mcause <= csr_wdata;(禁止软件写入)
           default: ;
         endcase
       end else if (debug.halted) begin
@@ -189,7 +185,8 @@ module CSR
         // mstatus.mpp <= MACHINE;
       end else if (trap.returned) begin
         mstatus.mpie <= 1'b1;
-        mstatus.mie  <= mstatus.mpie;
+        mstatus.mie <= mstatus.mpie;
+        mcause <= trap.new_mcause;
       end
     end
   end
@@ -198,10 +195,10 @@ module CSR
   always_ff @(posedge clk) begin
     if (atomic_rw_en && rwaddr.mode == 2'b00) begin
       unique case (short_addr)
+        8'h04:   mie <= {wdata[11], wdata[7], wdata[3]};
+        8'h05:   mtvec <= wdata;
         8'h40:   mscratch <= wdata;
         8'h41:   mepc <= wdata[CFG.XLEN-1:CFG.PC_ZEROS];  // (允许软件写入，通常用于ecall)
-        // 8'h42: mcause <= csr_wdata;(禁止软件写入)
-        // 8'h43: mtval <= csr_wdata;(只读)
         // 8'h44: mip <= csr_wdata;(只读)
         default: ;
       endcase
@@ -212,9 +209,7 @@ module CSR
     end else if (debug.halt) begin
       dpc <= debug.new_dpc;
     end else if (trap.occurred) begin
-      mepc   <= trap.new_mepc;
-      mcause <= trap.new_mcause;
-      // mtval <= new_mtval;
+      mepc <= trap.new_mepc;
     end
   end
 

ACT4/run.py
@@ -16,7 +16,6 @@ ENTRY = "rvtest_entry_point"
 RISCV_ARCH_TEST = Path("./riscv-arch-test")
 DUT_CONFIG = Path("./xt_rv32i")
 HDL = Path("./xt_rv32i_sim/HDL")
-VERILATOR_CPP = Path("./xt_rv32i_sim/verilator_cpp")
 
 # Pin to front(.sv) 置顶文件名数组
 # Verilog对文件出现的顺序很严格，某些文件必须放到列表最前面
@@ -42,7 +41,7 @@ def main():
         )
     ENTRY_POINT = get_symbols(elf_file, [ENTRY], OBJDUMP_EXE).get(ENTRY)
     if ENTRY_POINT is None:
-        raise Exception("The symbol {ENTRY} was not found. 没有找到 {ENTRY} 符号")
+        raise Exception(f"The symbol {ENTRY} was not found. 没有找到 {ENTRY} 符号")
 
     exe = build_verilator_simulation_exe(ENTRY_POINT)
     run_test(ENTRY_POINT, ELFS_DIR, OBJDUMP_EXE, exe, MAX_CYCLES_FACTOR)
@@ -85,11 +84,11 @@ def build_verilator_simulation_exe(ram_base: int):
     non_pkgs = [p for p in sv_files if p not in pkgs]
     all_files = [str(file.resolve()) for file in (pkgs + non_pkgs)]
 
-    (VERILATOR_CPP / "rtl.files").write_text("\n".join(all_files), "utf-8")
+    Path("rtl.files").write_text("\n".join(all_files), "utf-8")
 
-    cmd = f"verilator {TRACE_ARGS} --top {TOP_TB} -f rtl.files -cc --exe --build sim_main.cpp -GRAM_WORD_DEPTH={RAM_WORD_DEPTH} -GRAM_BASE_ADDR={ram_base}".split()
-    subprocess.run(cmd, cwd=VERILATOR_CPP, text=True, check=True)
-    return VERILATOR_CPP / "obj_dir" / f"V{TOP_TB}"
+    cmd = f"verilator {TRACE_ARGS} --top {TOP_TB} -f rtl.files --binary -GRAM_WORD_DEPTH={RAM_WORD_DEPTH} -GRAM_BASE_ADDR={ram_base}".split()
+    subprocess.run(cmd, text=True, check=True)
+    return Path("obj_dir", f"V{TOP_TB}")
 
 
 # 执行测试
@@ -123,12 +122,9 @@ def run_test(
         signature.parent.mkdir(parents=True, exist_ok=True)
         hex.parent.mkdir(parents=True, exist_ok=True)
         # 转换为16进制文本文件
+        verilog_args = f"-O verilog --verilog-data-width 4 --change-addresses -{entry_point:#X}".split()
         subprocess.run(
-            [str(OBJCOPY_EXE)]
-            + f"-O verilog --verilog-data-width 4 --change-addresses -{entry_point:#X}".split()
-            + [str(elf)]
-            + [str(hex)],
-            check=True,
+            [str(OBJCOPY_EXE), *verilog_args, str(elf), str(hex)], check=True
         )
         # 运行仿真
         cmd = [

ACT4/xt_rv32i_sim/HDL/tb/ActRam.sv
@@ -14,7 +14,7 @@ module ActRam #(
 
   logic [3:0][7:0] ram[WORD_DEPTH];
   initial begin
-    string firmware_file = "firmware.hex";
+    static string firmware_file = "firmware.hex";
     $value$plusargs("firmware=%s", firmware_file);
     $readmemh(firmware_file, ram);  // 从 0 开始全加载
   end

ACT4/xt_rv32i_sim/HDL/tb/XT_RV32I_Act_tb.sv
@@ -3,10 +3,32 @@ module XT_RV32I_Act_tb
 #(
     parameter int unsigned RAM_WORD_DEPTH = 32'h000F_0000,
     parameter int unsigned RAM_BASE_ADDR  = 32'h80000000
-) (
-    input clk,
-    input rst
-);
+) ();
+
+  // 时钟生成(周期为两个时间单位)
+  logic clk, rst;
+  always begin
+    clk = 0;
+    #1;
+    clk = 1;
+    #1;
+  end
+  // 复位与超时检测逻辑
+  initial begin
+    rst = 1;
+    #4;
+    rst = 0;
+  end
+
+
+  // 调试器接口
+  dm_hart_minimal_if dm_hart ();
+  assign dm_hart.ackhavereset = 0;
+  assign dm_hart.haltreq = 0;
+  assign dm_hart.resumereq = 0;
+  dm_register_if command0 ();
+  assign command0.transfer = 0;
+  assign command0.write = 0;
 
   wire stall_req = 0;
   wire core_stall_n;
@@ -21,6 +43,7 @@ module XT_RV32I_Act_tb
   wire msoftware_int = 0;
   wire mtimer_int = 0;
   wire [30:0] custom_int_code = 0;
+  int_source_if mint (.*);
   RISC_V_Core #(
       .CFG(CORE_DEFAULT_CFG),
       .INST_FETCH_REG(0),
@@ -55,7 +78,7 @@ module XT_RV32I_Act_tb
 
   always @(posedge clk, posedge rst) begin
     if (rst) begin
-      htif.check_halt <= 0;
+      htif.check_halt = 0;
     end else begin
       htif.capture_write();
     end

ACT4/xt_rv32i_sim/verilator_cpp/sim_main.cpp
@@ -1,29 +1,0 @@
-#include "VXT_RV32I_Act_tb.h"
-#include "verilated.h"
-#include <iostream>
-
-int main(int argc, char** argv) {
-    Verilated::commandArgs(argc, argv);
-    Verilated::traceEverOn(true);
-
-    auto top = VXT_RV32I_Act_tb();// 初始化模块
-
-    // 复位
-    Verilated::timeInc(1);
-    top.clk = 0;
-    top.rst = 1;
-    top.eval();
-    for (size_t i = 0; i < 4; i++) {
-        Verilated::timeInc(1);
-        top.clk = !top.clk;
-        top.eval();
-    }
-    top.rst = 0;
-    // 测试
-    while (!Verilated::gotFinish()) {
-        Verilated::timeInc(1);
-        top.clk = !top.clk;
-        top.eval();
-    }
-    return 0;
-}

RTL/RISC-V/Core/CoreReg.sv
@@ -23,15 +23,22 @@ module CoreReg
   logic wen;
   logic [4:0] waddr;
   logic [CFG.XLEN-1:0] wdata;
+
+  logic [4:0] raddr1;
+  assign debug_read_gpr.data = read_rs1.data;
   always_comb begin
     if (debug_override_gpr) begin
-      wen   = debug_write_gpr.en;
+      wen = debug_write_gpr.en;
       waddr = debug_write_gpr.addr;
       wdata = debug_write_gpr.data;
+
+      raddr1 = debug_write_gpr.addr;
     end else begin
-      wen   = write_rd.en;
+      wen = write_rd.en;
       waddr = write_rd.addr;
       wdata = write_rd.data;
+
+      raddr1 = read_rs1.addr;
     end
   end
 
@@ -40,12 +47,12 @@ module CoreReg
 
   //----------数据控制----------//
   always_comb begin
-    if (read_rs1.addr == 0) begin
+    if (raddr1 == 0) begin
       read_rs1.data = 0;
-    end else if (wen && read_rs1.addr == waddr) begin
+    end else if (wen && raddr1 == waddr) begin
       read_rs1.data = wdata;
     end else begin
-      read_rs1.data = core_reg[read_rs1.addr];
+      read_rs1.data = core_reg[raddr1];
     end
   end
 
@@ -59,13 +66,6 @@ module CoreReg
     end
   end
 
-  always_comb begin
-    if (debug_read_gpr.addr == 0) begin
-      debug_read_gpr.data = 0;
-    end else begin
-      debug_read_gpr.data = core_reg[debug_read_gpr.addr];
-    end
-  end
 
   always_ff @(posedge clk) begin
     if (!rst && (stall_n || debug.halted) && wen && waddr != 0) begin

RTL/RISC-V/Core/InstructionDecode.sv
@@ -54,7 +54,7 @@ module InstructionDecode
     id_out.reg_wen = 0;
 
     // 不可能同时读/写，地址计算可以共用加法器
-    access_addr_imm = imm_i;
+    access_addr_imm = 'x;
     id_out.load = 0;
     id_out.store = 0;
     id_out.load_addr = access_addr;
@@ -113,13 +113,17 @@ module InstructionDecode
         endcase
       end
       RV32I_OP_I: begin
-        id_out.reg_wen  = 1;
-        id_out.operand1 = read_rs1.data;
+        id_out.reg_wen = 1;
+        // 位反转，利用右移来实现左移
+        if (funct3 == RV32I_SLLI) id_out.operand1 = {<<{read_rs1.data}};
+        else id_out.operand1 = read_rs1.data;
         id_out.operand2 = imm_i;
       end
       RV32I_OP_R: begin
-        id_out.reg_wen  = 1;
-        id_out.operand1 = read_rs1.data;
+        id_out.reg_wen = 1;
+        // 位反转，利用右移来实现左移
+        if (funct3 == RV32I_SLL) id_out.operand1 = {<<{read_rs1.data}};
+        else id_out.operand1 = read_rs1.data;
         id_out.operand2 = read_rs2.data;
       end
       RV32I_OP_SYSTEM: begin

RTL/RISC-V/Core/InstructionExecute.sv
@@ -58,20 +58,18 @@ module InstructionExecute
   wire [31:0] alu_xor = operand1 ^ operand2;
   wire [31:0] alu_or = operand1 | operand2;
   wire [31:0] alu_and = operand1 & operand2;
-  wire [31:0] alu_shift_left = operand1 << operand2[4:0];
   wire [31:0] alu_shift_right_l = operand1 >> operand2[4:0];
   wire [31:0] alu_shift_right_a = $signed(operand1) >>> operand2[4:0];
+  wire [31:0] alu_shift_left = {<<{alu_shift_right_l}};  // 位反转，利用右移来实现左移
   wire [31:0] alu_base_addr_offset = id_ex_inst.addr + imm_b;
 
   wire alu_equal = operand1 == operand2;
-  wire alu_less_signed = $signed(operand1) < $signed(operand2);
+  // 优化比较器，共用资源
+  wire sign_diff = operand1[CFG.XLEN-1] ^ operand2[CFG.XLEN-1];
   wire alu_less_unsigned = operand1 < operand2;
-  logic alu_less;
-  always_comb begin
-    // 实际上SLT和SLTI的funct3相同
-    if (funct3 == RV32I_SLT || funct3 == RV32I_SLTI) alu_less = alu_less_signed;
-    else alu_less = alu_less_unsigned;
-  end
+  wire alu_less_signed = sign_diff ? operand1[CFG.XLEN-1] : alu_less_unsigned;
+  // 实际上SLT和SLTI的funct3相同
+  wire alu_less = (funct3 == RV32I_SLT) ? alu_less_signed : alu_less_unsigned;
 
   // Load指令访存数据的高位扩展
   logic extension_bit;

commit c273a6231d5f6581d171fdb5a020019de349cb86

修复了mcause在自陷时未被赋值的问题
此bug在 6e5ab3e0f7 被引入

Fixes: 6e5ab3e0f7

RTL/RISC-V/Core/CSR.sv
@@ -181,12 +181,12 @@ module CSR
         dcsr.cause <= debug.new_cause;
       end else if (trap.occurred) begin
         mstatus.mpie <= mstatus.mie;
-        mstatus.mie  <= 0;
+        mstatus.mie <= 0;
         // mstatus.mpp <= MACHINE;
+        mcause <= trap.new_mcause;
       end else if (trap.returned) begin
         mstatus.mpie <= 1'b1;
-        mstatus.mie <= mstatus.mpie;
-        mcause <= trap.new_mcause;
+        mstatus.mie  <= mstatus.mpie;
       end
     end
   end